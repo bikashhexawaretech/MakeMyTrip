52b8d01c2a643527e9b8df185f4c23b8093ff288
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-03-14 11:02:59 +0530

    Commit

--- index.js
+++ index.js
@@ -8,9 +8,77 @@ var portC = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 var fs = require("fs");
-
+var facebookResponse='';
  
- 
+app.post('/',function(req,res){
+      if(req.body.result.action==='ActionIncident'){
+      
+      if(req.body.result.parameters.entityCategory==='')
+      {
+        facebookResponse={
+            "speech": "",
+          "messages": [
+            {
+              "type": 1,
+              "platform": "facebook",
+              "title": "Select Category",
+              "subtitle": "",
+              "imageUrl": "http://www.cromacampus.com/wp-content/uploads/2017/05/servicenow-tool-training.png",
+              "buttons": [
+                {
+                  "text": "Hardware",
+                  "postback": "Hardware"
+                },
+                {
+                  "text": "Software",
+                  "postback": "Software"
+                },
+                {
+                  "text": "Network",
+                  "postback": "Network"
+                }
+              ]
+            }
+            
+            
+          ]
+         }
+         return res.json(facebookResponse);
+      }
+      if(req.body.result.parameters.entitySeverity==='')
+      {
+        facebookResponse={
+            "speech": "",
+          "messages": [
+            {
+              "type": 1,
+              "platform": "facebook",
+              "title": "Select Urgency",
+              "subtitle": "",
+              "imageUrl": "http://www.cromacampus.com/wp-content/uploads/2017/05/servicenow-tool-training.png",
+              "buttons": [
+                {
+                  "text": "High",
+                  "postback": "High"
+                },
+                {
+                  "text": "Low",
+                  "postback": "Low"
+                },
+                {
+                  "text": "Medium",
+                  "postback": "Medium"
+                }
+              ]
+            }
+            
+            
+          ]
+         }
+         return res.json(facebookResponse);
+      }
+    }
+    });
  
 
 app.listen(portC, function(){
