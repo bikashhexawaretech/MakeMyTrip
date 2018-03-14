f57800cae18694f6570012ea12cdb988ced329e6
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-03-14 10:46:32 +0530

    Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+'use strict';
+var request = require('http');
+var express=require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var portC = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+var fs = require("fs");
+
+app.get('/',function(req,res){
+console.log('Success');
+  
+})
+ 
+ 
+
+app.listen(portC, function(){
+    console.log('AGENT is running my app on  PORT: ' + portC);
+});
+
+
